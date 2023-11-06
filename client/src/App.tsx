85d8ef9060e4304e223ddecf8b127bb34e1b7f6e
elwshen <elwshen@gmail.com>
2023-11-05 23:25:28 -0500

    make chat box fixed height

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+  <div className="App">
       <div className="messages">
         {messages.map((message, index) => (
           <div
